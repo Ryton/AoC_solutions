d18c26677728a7fa4151793be4e565aa43707934
J. Verh. <Joachim.Verhelst@gmail.com>
2025-08-19 08:23:09 +0200

    moved floodfill to helperfunction lib & some cleanup

--- 2024/helperfunctions.py
+++ 2024/helperfunctions.py
@@ -33,3 +33,133 @@ def toc(tempBool=True, what=""):
 def tic():
     # Records a time in TicToc, marks the beginning of a time interval
     toc(False)
+
+class Map(dict):
+    def __init__(self, *args, **kwargs):
+        super(Map, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.items():
+                    if isinstance(v, dict):
+                        v = Map(v)
+                    if isinstance(v, list):
+                        self.__convert(v)
+                    self[k] = v
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if isinstance(v, dict):
+                    v = Map(v)
+                elif isinstance(v, list):
+                    self.__convert(v)
+                self[k] = v
+
+    def __convert(self, v):
+        for elem in range(0, len(v)):
+            if isinstance(v[elem], dict):
+                v[elem] = Map(v[elem])
+            elif isinstance(v[elem], list):
+                self.__convert(v[elem])
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(Map, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(Map, self).__delitem__(key)
+        del self.__dict__[key]
+
+
+
+
+def floodfill(grid,start=(0,0),obstructed_value = 9999):
+    
+    gridsize = np.shape(grid)+ np.array([2,2]) #with walls.
+    nrows, ncols = np.shape(grid)
+    max_tries = np.max([obstructed_value,nrows*ncols])
+
+    # naive, visit them all again... but only need to re-visit ones next to values that have been updated.
+    #for n in np.arange(0,max_tries):
+    n = 0
+    if 1: #
+        if n ==0:
+            # init  surrounding 
+            expandgrid = np.ones(gridsize)*max_tries
+            expandgrid[0,:]  =  max_tries*2 +1
+            expandgrid[-1,:]  =  max_tries*2 +1
+            expandgrid[:,0]  =  max_tries*2 +1
+            expandgrid[:,-1]  =  max_tries*2 +1
+
+            tovisit_again = [] ##only visit again if adj was updated!
+            possiblepath = []
+            for a in np.arange(1,nrows+1):
+                for b in np.arange(1,ncols+1):
+                
+                    if grid[a-1,b-1]<  0 :
+                        expandgrid[a,b] = max_tries +1
+                    else:
+                        possiblepath.append(copy.copy((int(a),int(b))))
+            expandgrid[start[0]+1,start[1]+1]= 0
+            #print(possibleroute)
+            # iterate over all positions.
+
+            # go over them once!    
+
+            a,b= start+np.array([1,1])
+            tovisit_again = [[a-1,b],[a+1,b],[a,b-1],[a,b+1]]
+            next_list = []
+        
+        # iterate till len_tovisit_again <0
+        
+        while ((len(tovisit_again)>0) and n<max_tries):
+            #print("listlength",len(tovisit_again))
+            #print(tovisit_again)
+            n = n+1
+            for loc in tovisit_again:
+                a,b = loc
+                
+                #grid3x3 = expandgrid[a-1:+a+1,b-1:b+1] #also diag ok
+                grid_cross = [expandgrid[a-1,b],expandgrid[a+1,b],expandgrid[a,b-1],expandgrid[a,b+1]]
+                minval = np.min(grid_cross)
+                
+                isedge = (a ==0 or a>nrows or b ==0 or b>ncols)
+                if not(isedge):
+                    iswall = (grid[a-1,b-1]<0)
+                else:
+                    iswall = isedge 
+
+                if (expandgrid[*loc] > minval+1) and not(iswall) and not(isedge):  
+                    expandgrid[*loc] = minval+1
+                    for adj in [[a-1,b],[a+1,b],[a,b-1],[a,b+1]]:
+                        #print("adj", adj)
+                        a,b = adj
+                        isedge = (a ==0 or a>nrows or b ==0 or b>ncols)
+                        if not(isedge):
+                            iswall = (grid[a-1,b-1])<0
+                        else:
+                            iswall = isedge
+                            
+                        if iswall or isedge: # then  a wall or side  (==> skip those
+                            pass
+                        else:
+                            #print("adj", adj)
+                            #print("val", expandgrid[*adj])
+                            #print("minval = ", minval) 
+                            if expandgrid[*adj]>(minval+1):
+                                next_list.append(adj)
+                                
+                
+            #remove duplicates
+            tovisit_again = copy.copy (next_list)
+            next_list = [] 
+    #print(expandgrid)
+    return expandgrid
--- 2024/y24_d18.py
+++ 2024/y24_d18.py
@@ -13,25 +13,24 @@ print(yd_[1])
 
 
 try: #try to get from filename
-    #fname = sys.argv[0] ### os.path.basename(__file__)
-
     yd_ =fname.split("_")
     year = 2000+ int(yd_[0][1:])
     day = int(yd_[1][1:-3]) # drop .py
-
 except:
     year = 2024
     day = 1 # change this!
-#print(f"puzzle for {day} - {year}:")
+
 
 inputtype  = "P" # D(emo) or P(ersonal)
-if inputtype == "D":
-    NBYTES = 12
-else: 
-    NBYTES = 1024
 submit = "none" #"a" , "b", "none"
-##### helperfuntions for this day
+
+ 
 ################################################### begin of today's solution ###################################################
+
+##### helperfuntions for this day
+
+#def floodfill() moved to helperfunctions
+
 def load_data(inputtype="D"): #["D","P"]
     obj = Map()
     demodata = """5,4
@@ -98,102 +97,19 @@ Do floodfill check of value for end square.
 Test w empty => 12.
 """
 
-def floodfill(grid,start=(0,0), nrows = 7, ncols = 7):
-    max_tries = 9999 #nrows*ncols
-    gridsize = np.shape(grid)+ np.array([2,2]) #with walls.
-
-    
-
-    # naive, visit them all again... but only need to re-visit ones next to values that have been updated.
-    #for n in np.arange(0,max_tries):
-    n = 0
-    if 1: #
-        if n ==0:
-            # init  surrounding 
-            expandgrid = np.ones(gridsize)*max_tries
-            expandgrid[0,:]  =  max_tries*2 +1
-            expandgrid[-1,:]  =  max_tries*2 +1
-            expandgrid[:,0]  =  max_tries*2 +1
-            expandgrid[:,-1]  =  max_tries*2 +1
-
-            tovisit_again = [] ##only visit again if adj was updated!
-            possiblepath = []
-            for a in np.arange(1,nrows+1):
-                for b in np.arange(1,ncols+1):
-                
-                    if grid[a-1,b-1]<  0 :
-                        expandgrid[a,b] = max_tries +1
-                    else:
-                        possiblepath.append(copy.copy((int(a),int(b))))
-            expandgrid[start[0]+1,start[1]+1]= 0
-            #print(possibleroute)
-            # iterate over all positions.
-
-            # go over them once!    
-
-            a,b= start+np.array([1,1])
-            tovisit_again = [[a-1,b],[a+1,b],[a,b-1],[a,b+1]]
-            next_list = []
-        
-        # iterate till len_tovisit_again <0
-        
-        while ((len(tovisit_again)>0) and n<max_tries):
-            #print("listlength",len(tovisit_again))
-            #print(tovisit_again)
-            n = n+1
-            for loc in tovisit_again:
-                a,b = loc
-                
-                #grid3x3 = expandgrid[a-1:+a+1,b-1:b+1] #also diag ok
-                grid_cross = [expandgrid[a-1,b],expandgrid[a+1,b],expandgrid[a,b-1],expandgrid[a,b+1]]
-                minval = np.min(grid_cross)
-                
-                isedge = (a ==0 or a>nrows or b ==0 or b>ncols)
-                if not(isedge):
-                    iswall = (grid[a-1,b-1]<0)
-                else:
-                    iswall = isedge 
-
-                if (expandgrid[*loc] > minval+1) and not(iswall) and not(isedge):  
-                    expandgrid[*loc] = minval+1
-                    for adj in [[a-1,b],[a+1,b],[a,b-1],[a,b+1]]:
-                        #print("adj", adj)
-                        a,b = adj
-                        isedge = (a ==0 or a>nrows or b ==0 or b>ncols)
-                        if not(isedge):
-                            iswall = (grid[a-1,b-1])<0
-                        else:
-                            iswall = isedge
-                            
-                        if iswall or isedge: # then  a wall or side  (==> skip those
-                            pass
-                        else:
-                            #print("adj", adj)
-                            #print("val", expandgrid[*adj])
-                            #print("minval = ", minval) 
-                            if expandgrid[*adj]>(minval+1):
-                                next_list.append(adj)
-                                
-                
-            #remove duplicates
-            tovisit_again = copy.copy (next_list)
-            next_list = [] 
-    #print(expandgrid)
-    return expandgrid
-
-def calc_a(grid, start,nrows, ncols):
-    expandgrid = floodfill(grid, start,nrows, ncols)
+def calc_a(grid, start):
+    expandgrid = floodfill(grid, start)
 
     return expandgrid[*data.goal] # answer a 
 
-def calc_b(data,NBYTES):
+def calc_b(data,NBYTES,NBYTES_MAX):
 
     ## would be faster with nomad or scipy. => one parametere minimisation.
-    for n in np.arange(NBYTES,NBYTES*5): # np.arange( 3035,3036+5):# 
+    for n in np.arange(NBYTES,NBYTES_MAX): # np.arange( 3035,3036+5):# 
 
         corrupted_grid = parse(data, nbytes = n)
         #print(corrupted_grid)
-        answer_a= calc_a(corrupted_grid, data.start , data.nrows, data.ncols)
+        answer_a= calc_a(corrupted_grid, data.start )
         print(n,answer_a, data.data.split("\n")[n-1])
         if answer_a >= 9999:
             return data.data.split("\n")[n-1] # answer b
@@ -204,8 +120,20 @@ def calc_b(data,NBYTES):
 
 ## main script when file is run as script. 
 if __name__ == "__main__":
+
+
     print(f"**** Day {day} *"+ "*"*14)
+    
+    if inputtype == "D":
+        NBYTES = 12
+    else: 
+        NBYTES = 1024
+        NBYTES_MAX = 1024 *3
+        
+    
+
     # preprocessing
+
     data =  load_data(inputtype=inputtype) #"D","P"
     
     
@@ -213,12 +141,12 @@ if __name__ == "__main__":
     #print(corrupted_grid)
     # timed exeution
     tic()
-    answer_a= calc_a(corrupted_grid, data.start , data.nrows, data.ncols)
+    answer_a= calc_a(corrupted_grid, data.start)
     t_a = toc()
 
     tic()
 
-    answer_b = calc_b(data,NBYTES)
+    answer_b = calc_b(data,NBYTES,NBYTES_MAX)
     t_b = toc()
 
     #printout
